2986be8a86ecd7315cc76e79633f11922c6e5e3a
Yago de Santana Ronchi <yago.ronchi@db1.com.br>
2026-03-06 16:10:23 -0300

    feat: exercicio 01 - calculo de IMC

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Exercício 1: Calculadora de IMC (Índice de Massa Corporal)
+// importação para utilização de ponto em vez de virgula.
+using System.Globalization;
+CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+
+// Entrada do peso do usuário
+Console.WriteLine("Digite seu peso (kg): ");
+double peso = Convert.ToDouble(Console.ReadLine()!);
+
+//Verificação para peso negativo ou zero
+if(peso <= 0)
+{
+    Console.WriteLine("Digite um peso maior que zero");
+    return;
+}
+
+// Entrada da altura do usuário
+Console.WriteLine("Digite sua altura (m): ");
+double altura = double.Parse(Console.ReadLine()!);
+
+// Verificação para alturas negativas ou zero
+if(altura <= 0)
+{
+    Console.WriteLine("Digite uma altura maior que zero");
+    return;
+}
+
+// Calculo do IMC
+double imc = peso / (altura*altura);
+Console.WriteLine($"Seu IMC é: {imc:F2} ");
