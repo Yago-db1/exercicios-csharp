e81ca3a3a29e7a40b3a0e129aad11efd64c54481
Yago de Santana Ronchi <yago.ronchi@db1.com.br>
2026-03-12 11:38:05 -0300

    Refatorando metodo para validar entrada do usuario

---
+++ Exercicio1/Exercicio1.cs
@@ -0,0 +1,14 @@
+namespace exercicios;
+
+public class Exercicio1
+{
+    public static void Executar()
+    {
+        Console.Clear();
+        Console.WriteLine("Bem vindo a Calculadora de IMC");
+        decimal peso = exercicios.ValidarEntradaUsuario.LerDecimal("Digite o seu peso (kg): ");
+        decimal altura = exercicios.ValidarEntradaUsuario.LerDecimal("Digite a sua altura(m): ");
+        decimal imc = peso / (altura * altura);
+        Console.WriteLine($"Seu IMC é: {imc:F2} ");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,26 +1,18 @@
-﻿//Exercício 1: Calculadora de IMC (Índice de Massa Corporal)
-// importação para utilização de ponto em vez de virgula.
-using System.Globalization;
+﻿using System.Globalization;
 CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
-// Entrada do peso e altura do usuario. 
-double peso = LerEntradaUsuario("Digite o seu peso (kg): ");
-double altura = LerEntradaUsuario("Digite a sua altura(m): ");
-// Calculo do IMC
-double imc = peso / (altura * altura);
-Console.WriteLine($"Seu IMC é: {imc:F2} ");
-// Método para validar a entrada do usuário
-double LerEntradaUsuario(string mensagem)
+Console.Clear();
+Console.WriteLine("=== MENU ===");
+Console.WriteLine("1 - Calculadora de IMC (Índice de Massa Corporal)");
+Console.WriteLine("2 - Para sair");
+Console.Write("Digite uma opção: ");
+int opcaoMenu = exercicios.ValidarEntradaUsuario.LerOpcaoMenuPrincipal();
+switch (opcaoMenu)
 {
-    double valor;
-    while (true)
-    {
-        Console.Write(mensagem);
-        string entradaUsuario = Console.ReadLine()!;
-        if (double.TryParse(entradaUsuario, out valor) && valor > 0)
-        {
-            return valor;
-        }
-        Console.WriteLine("Digite um valor maior que zero");
-    }
+    case 1:
+        exercicios.Exercicio1.Executar();
+        break;
+    case 2:
+        Console.WriteLine("Saindo...");
+        return;
 }
