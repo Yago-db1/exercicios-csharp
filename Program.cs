ce3df0745c7fa9812811ab0171b45319df3a56e1
Yago de Santana Ronchi <yago.ronchi@db1.com.br>
2026-03-12 08:16:59 -0300

    refactor(program.cs): refactoring program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+Console.Clear();
+Console.WriteLine("=== MENU ===");
+Console.WriteLine("1 - Calculadora de IMC (Índice de Massa Corporal)");
+Console.WriteLine("2 - Para sair");
+Console.Write("Digite uma opção: ");
+int opcaoMenu = ValidarEntradaUsuario();
+switch (opcaoMenu)
+{
+    case 1:
+        exercicios.Exercicio1.Executar();
+        break;
+    case 2:
+        Console.WriteLine("Saindo...");
+        return;
+}
+int ValidarEntradaUsuario()
+{
+    int opcao;
+    while (true)
+    {
+        string entradaUsuario = Console.ReadLine()!;
+
+        if (int.TryParse(entradaUsuario, out opcao) && opcao >= 1 && opcao <= 2)
+        {
+            return opcao;
+        }
+
+        Console.Write("Digite uma opção válida (1 ou 2): ");
+    }
+}
