e16ddd8292df62604d7d02027098214a406a479c
Yago de Santana Ronchi <yago.ronchi@db1.com.br>
2026-03-17 08:41:44 -0300

    validando se o divisor for zero e trocando condicional no while

--- Exercicio3/Exercicio3.cs
+++ Exercicio3/Exercicio3.cs
@@ -4,15 +4,10 @@ public class Exercicio3
 {
     public static void Calculadora()
     {
-        while (true)
+        OpcoesMenu.ExibirMenuCalculadora();
+        int opcaoCalculadora = ValidarEntradaUsuario.LerOpcaoMenuCalculadora();
+        while (opcaoCalculadora !=5)
         {
-            OpcoesMenu.ExibirMenuCalculadora();
-            int opcaoCalculadora = ValidarEntradaUsuario.LerOpcaoMenuCalculadora();
-            if (opcaoCalculadora == 5)
-            {
-                Console.WriteLine("Saindo...");
-                return;
-            }
             Console.Write("Digite o primeiro número: ");
             decimal numero1 = ValidarEntradaUsuario.LerNumeroCalculadora();
             Console.Write("Digite o segundo número: ");
@@ -30,17 +25,14 @@ public class Exercicio3
                     resultado = OperacoesCalculadora.Multiplicar(numero1, numero2);
                     break;
                 case 4:
-                    while (numero2 == 0)
-                    {
-                        Console.Write("Digite um valor diferente de zero: ");
-                        numero2 = ValidarEntradaUsuario.LerNumeroCalculadora();
-                    }
                     resultado = OperacoesCalculadora.Dividir(numero1, numero2);
                     break;
             }
             Console.WriteLine($"Resultado: {resultado}");
             Console.WriteLine("Pressione qualquer tecla para continuar...");
             Console.ReadKey();
+            OpcoesMenu.ExibirMenuCalculadora();
+            opcaoCalculadora = ValidarEntradaUsuario.LerOpcaoMenuCalculadora();
         }
     }
 }
--- OperacoesCalculadora/OperacoesCalculadora.cs
+++ OperacoesCalculadora/OperacoesCalculadora.cs
@@ -16,6 +16,11 @@ public class OperacoesCalculadora
     }
     public static decimal Dividir(decimal numero1, decimal numero2)
     {
+        while(numero2 ==0)
+        {
+            Console.Write("Digite um valor diferente de zero: ");
+            numero2 = ValidarEntradaUsuario.LerNumeroCalculadora();
+        }
         return numero1 / numero2;
     }
 }
